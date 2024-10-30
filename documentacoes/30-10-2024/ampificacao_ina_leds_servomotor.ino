c7e876ef14750bfa26921fb865c64066d4e5ae23
Bruno <brunohoffmann.2003@gmail.com>
2024-10-30 16:04:35 -0300

    Adiciona os arquivos referentes ao dia 30-10-2024

---
+++ documentacoes/30-10-2024/ampificacao_ina_leds_servomotor.ino
@@ -0,0 +1,31 @@
+#include <ESP32Servo.h>
+int pino_servo = 22;
+Servo servo;
+
+#define pin_ci 34
+#define pin_led 23
+int valor_amplificado = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(34, INPUT);
+  pinMode(35, OUTPUT);
+  servo.attach(pino_servo);
+  
+
+}
+
+void loop() {
+  valor_amplificado = analogRead(pin_ci);
+  Serial.print("Tensao do amplificador: ");
+  Serial.println(valor_amplificado);
+  int brilho_led = map(valor_amplificado, 0, 4095, 0, 255);
+
+  analogWrite(pin_led, brilho_led);
+
+  int sla_servo = map(valor_amplificado, 0, 4095, 0, 180);
+  servo.write(sla_servo);
+  Serial.print("Valor do servo: ");
+  Serial.println(sla_servo);
+  delay(20);
+}
